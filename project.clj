eddd6c3b872f13e4ff949fd53aeb9d840dc629fc
tuddman <scott.tudd@gmail.com>
2018-05-29 23:15:00 -0400

    updates dependency version

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [base64-clj "0.1.1"]
                  [cheshire "5.8.0"]
-                 [clj-http "3.7.0"]
+                 [clj-http "3.9.0"]
                  [clj.qrgen "0.4.0"]
                  [environ "1.1.0"]
                  [com.taoensso/timbre "4.10.0"]]
